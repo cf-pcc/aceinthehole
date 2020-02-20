8e6f21734ae6541102c0152d061a86fb21dc01e5
cf-pcc <61267325+cf-pcc@users.noreply.github.com>
2020-02-19 23:26:37 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!--
+File Name: footer.php
+Date: 02/19/20
+Author: Craig Fouts
+-->
+
+<?php
+echo
+"
+<p>Copyright Information</p>
+<p>Contact Information</p>
+";
+?>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!--
+File Name: header.php
+Date: 02/19/20
+Author: Craig Fouts
+-->
+
+<?php
+echo
+"
+<img src='images/AITHME_red_and_black_on_transparent_Impact.png' alt='aceinthehole'>
+<input class='menu-btn' type='checkbox' id='menu-btn' />
+<label class='menu-icon' for='menu-btn'><span class='navicon'></span></label>
+    <ul class='menu'>
+        <nav>
+            <li><a href='http://craigfouts.webhostingforstudents.com/cas222/template/template.html'>Home</a></li>
+            <li><a href=''>Events</a></li>
+            <li><a href=''>Gallery</a></li>
+            <li><a href=''>Contact Us</a></li>
+        </nav>
+        </ul>
+";
+?>
